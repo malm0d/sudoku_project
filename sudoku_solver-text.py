386671ae5f8e91b9637d5d92f00d7848364ae44f
Malcolm Tan <zherui_tan@live.com.sg>
2020-05-24 08:55:43 -0400

    Comments for board structure for Sudoku Project (text ver).

---
+++ sudoku_solver-text.py
@@ -0,0 +1,17 @@
+# board = [
+# [0, 1, 2, 3, 4, 5, 6, 7, 8],
+# [0, 1, 2, 3, 4, 5, 6, 7, 8],
+# [0, 1, 2, 3, 4, 5, 6, 7, 8],
+#------------------------------
+# [0, 1, 2, 3, 4, 5, 6, 7, 8],
+# [0, 1, 2, 3, 4, 5, 6, 7, 8],
+# [0, 1, 2, 3, 4, 5, 6, 7, 8],
+#------------------------------
+# [0, 1, 2, 3, 4, 5, 6, 7, 8],
+# [0, 1, 2, 3, 4, 5, 6, 7, 8],
+# [0, 1, 2, 3, 4, 5, 6, 7, 8],
+#------------------------------
+# ]
+#Navigate Sudoku board by board[row][column]
+#Vertical lines to separate after board[row][2]; and horizontal lines to separate after board[2]
+#Vertical and horizontal lines to define the 9units x 9units Sudoku grid (81 aquares total).
