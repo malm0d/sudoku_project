88c3ab832b46bba73d167404a741883521b91d77
Malcolm Tan <zherui_tan@live.com.sg>
2020-06-02 01:32:51 -0400

    Started Defineing new function that checks if the Sudoku board is globally correct. Row and Column defined, yet to define for Subgrids.

--- sudoku_txtv1.py
+++ sudoku_txtv1.py
@@ -50,6 +50,7 @@ def make_board(board):
         else:
             continue
 
+
 #define function that allows us to locate position of all empty cells,
 #by returning a list of coordinates of empty cells.
 def find_empty_cells(board):
@@ -57,10 +58,30 @@ def find_empty_cells(board):
     for row in range(len(board)):
         for column in range(len(board[row])):
             if board[row][column] == 0:                     #an empty cell is denoted by the integer (neutral) zero.
-                empty_cell = (row, column)                  #empty_cell holds the coordinates of an empty cell, as in (x, y), in the form of a Tuple.
+                empty_cell = (row, column)                  #empty_cell holds the coordinates of an empty cell, as in (row, col), in the form of a Tuple.
                 lst.append(empty_cell)                      #appends each Tuple of coordinates to the list object (lst) that was constructed in the function.
     print (lst)
 
+
+#define function that checks if the solution to the board is globally correct.
+#This means, we need to check, for the cell that we are working on, there is harmony across the cell's row.
+#We also need to check, for the cell that we are working on,there is harmony down the cell's column.
+#And we also need to check, for the cell we are working on, there is harmony within each subgrid that the cell is a part of.
+
+def harmony(board, choice, emptycell_coord):                #choice argument is the number we want to put in an empty cell. And emptycell_coord argument is a Tuple (row, column).
+    #check harmony in row:
+    for x in range(len(board[0])):                                                      #board has a fixed length, so we can opt for board[0] as every row in the board will yield range(9)->[0,1,..,7,8].
+        if board[emptycell_coord[0]][x] == choice and x != emptycell_coord[1]:          #access x-coord of empty cell via index notation: emptycell_coord[0] = row. The second conditional: x != emptycell_coord[1], allows the function to
+            return False                                                                #skip checking the empty cell against the empty cell. Since every empty cell will be x == emptycell_coord[1] and the function is going to iterate through the
+                                                                                        #entire row, it needs to skip the empty cell and not return False if we just inserted a number into an empty cell and are checking for duplicates in the row. 
+                                                                                        #Conditionals will be met if there are duplicates in the row and x is not the column of an empty cell; only then harmony function returns False.
+    #check harmony in column:                                                           
+    for y in range(len(board)):                                                         #yields: [0,1,2,3,4,5,6,7,8], since len(board) is 9. This checks for harmony every column down the nine rows.
+        if board[y][emptycell_coord[1]] == choice and y != emptycell_coord[0]:          #access y-coor of empty cell via index notation: emptycell_coord[1] = column. The second conditional: y != emptycell_coord[0], allows the function to 
+            return False                                                                #skip checking the empty cell against the empty cell. Every empty cell will be y == emptycell_coord[0], so function needs to be able to skip the empty cell
+                                                                                        #and not return False if we just inserted a number into an empty cell. If condtionals are both met, returns False, meaning there is no harmony in the column.
+
+    #check harmony in subgrid:
 
 
 make_board(sudoku_board)
