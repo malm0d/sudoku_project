5432a6d195139ef401ed285d72c87c4b1df6dd07
Malcolm Tan <zherui_tan@live.com.sg>
2020-06-02 09:41:47 -0400

    Defined function that checks Sudoku board globally, function checks rows, columns, and subgrids.

--- sudoku_txtv1.py
+++ sudoku_txtv1.py
@@ -69,19 +69,29 @@ def find_empty_cells(board):
 #And we also need to check, for the cell we are working on, there is harmony within each subgrid that the cell is a part of.
 
 def harmony(board, choice, emptycell_coord):                #choice argument is the number we want to put in an empty cell. And emptycell_coord argument is a Tuple (row, column).
-    #check harmony in row:
+    #check harmony in row of empty cell::
     for x in range(len(board[0])):                                                      #board has a fixed length, so we can opt for board[0] as every row in the board will yield range(9)->[0,1,..,7,8].
         if board[emptycell_coord[0]][x] == choice and x != emptycell_coord[1]:          #access x-coord of empty cell via index notation: emptycell_coord[0] = row. The second conditional: x != emptycell_coord[1], allows the function to
             return False                                                                #skip checking the empty cell against the empty cell. Since every empty cell will be x == emptycell_coord[1] and the function is going to iterate through the
                                                                                         #entire row, it needs to skip the empty cell and not return False if we just inserted a number into an empty cell and are checking for duplicates in the row. 
                                                                                         #Conditionals will be met if there are duplicates in the row and x is not the column of an empty cell; only then harmony function returns False.
-    #check harmony in column:                                                           
+    #check harmony in column of empty cell:                                                           
     for y in range(len(board)):                                                         #yields: [0,1,2,3,4,5,6,7,8], since len(board) is 9. This checks for harmony every column down the nine rows.
         if board[y][emptycell_coord[1]] == choice and y != emptycell_coord[0]:          #access y-coor of empty cell via index notation: emptycell_coord[1] = column. The second conditional: y != emptycell_coord[0], allows the function to 
             return False                                                                #skip checking the empty cell against the empty cell. Every empty cell will be y == emptycell_coord[0], so function needs to be able to skip the empty cell
                                                                                         #and not return False if we just inserted a number into an empty cell. If condtionals are both met, returns False, meaning there is no harmony in the column.
+    #check harmony in subgrid of empty cell:
+    subgrid_row = emptycell_coord[0] // 3                                   #since there are 3 subgrids row-wise & column-wise, group each row & column into 3 subgrids, and each subgrid is composed of 3 rows and 3 columns.
+    subgrid_column = emptycell_coord[1] // 3                                #floor division (//) returns only the quotient (integer) of the division. So, (0 to 2) // 3 = 0; (3 to 5) // 3 = 1; (6 to 8) // 3 = 2.
+                                                                            #Subgrids are, starting from top leftmost of Sudoku board (row, column): (0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2).
+    #subgrid row & col value falls as either: 0, 1, or 2.
+    for row in range(subgrid_row * 3, (subgrid_row * 3) + 3):                 #checks subgrid in downwards y direction. For subgrid_row = 0, 1, 2, range = (0, 3), (3, 6), (6, 9) respectively.
+        for col in range (subgrid_column * 3, (subgrid_column * 3) + 3):      #checks subgrid in forward x direction. For subgrid_column = 0, 1, 2, range = (0, 3), (3, 6), (6, 9) respectively.
+            if board[row][col] == choice and (row, col) != emptycell_coord:   #same as before, we want to skip checking the empty cell against the empty cell, as every empty cell will be (row, col) == emptycell_coord.
+                return False
 
-    #check harmony in subgrid:
+    return True                                                             #if the arguments do not trigger any return False, we know that the current board is valid, globally.
+
 
 
 make_board(sudoku_board)
